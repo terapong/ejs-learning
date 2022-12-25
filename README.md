b10d9f9917b4fa789535686f9c67cfdec9eb88db
terapong <terapong.p@gmail.com>
2022-12-25 11:24:38 +0700

    error หว่ะ ค่อยว่ากัน น่ะ

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ejs-learning
+node app.js
+node i nodemon
+
+"scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "dev": "nodemon app.js"
+  }
+//ใช้ตัวนี้ แทน node app.js น่ะ
+mpm run dev
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+
+</html>
--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express')
-const app = expess()
+const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
 
+const user = {
+    firstName: 'Ying toto หกดกห sddsd',
+    lastName: 'potisuwan ewewee',
+    admin: true
+}
+
+const posts = [
+    {title: 'Title 1', bory: 'Test is body 1'},
+    {title: 'Title 2', bory: 'Test is body 2'},
+    {title: 'Title 3', bory: 'Test is body 3'},
+    {title: 'Title 4', bory: 'Test is body 4'},
+]
+
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('pages/index', {
+        user: user,
+        title: "Home Page"
+    })
+})
+
+app.get('/articles', (req, res) => {
+    res.render('pages/articles', {
+        articles: posts,
+        title: "Articles"
+    })
+    
 })
     
+app.listen(port, () => {
+    console.log(`App listening at port ${port}`)
+})
